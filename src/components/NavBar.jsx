7f1d89e1bf6992839cfa27acaea09454909e2ec8
afnessa <af.nesss07@gmail.com>
2025-09-04 17:34:14 +0200

    btns nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import logo from '../assets/images/logo.jpg'
 
 
 const NavBar = () => {
+  const navLinkClass = ({ isActive }) =>
+    `${isActive ? "bg-black" : ""} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`;
+
   return (
   <nav className="bg-indigo-700 border-b border-indigo-500"><div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="flex h-20 items-center justify-between">
@@ -12,9 +15,9 @@ const NavBar = () => {
         <span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span></Link>
           <div className="md:ml-auto">
             <div className="flex space-x-2">
-            <Link to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</Link>
-            <Link to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</Link>
-            <Link to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Job</Link>
+            <NavLink to="/"   className={navLinkClass}>Home</NavLink>
+            <NavLink to="/jobs" className={navLinkClass}>Jobs</NavLink>
+            <NavLink to="/add-job" className={navLinkClass}>Add Job</NavLink>
             </div>
           </div>
         </div>
