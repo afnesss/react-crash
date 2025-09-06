0c650bb23abc8ec1088647e746672a6f6c5f5e46
afnessa <af.nesss07@gmail.com>
2025-09-06 22:45:57 +0200

    job fetch dynamic page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,47 +1,75 @@
 import { Link } from "react-router-dom"
 import { useLocation } from "react-router-dom"
+import {useState, useEffect} from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import Spinner from "../components/Spinner"
 
 const JobPage = () => {
   const location = useLocation();
-  location.pathname
+  const [job, setJob] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async() => {
+      try {
+        const id = location.pathname.split("/").pop(); // якщо url = /jobs/1 → id = 1
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log('error fetching job ' + error)
+      }
+      finally{
+        setLoading(false);
+      }
+
+    }
+    fetchJob();
+  }, [location.pathname])
+
   return (
     <>
+    
       <div className="container m-auto py-6 px-7">
         <Link to="/jobs" className="text-indigo-500">Back to Job Listing</Link>
       </div>
 
+         {loading? (    
+            <div className="flex justify-center mt-10">
+            <Spinner loading={loading} />
+            </div>) : (
+              <>
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6 ">
           <div className="bg-white shadow-md p-6 rounded-xl mb-6">
             <div>
-              Full-Time
+              {job.type}
             </div>
             <div className="text-3xl font-bold mt-4">
-              Senior React Developer
+              {job.title}
             </div>
         <div className="text-orange-700 mt-5 mb-3">
         <FaMapMarker className="inline text-lg mb-1 mr-1"/>
-        Boston, MA</div>
+        {job.location}</div>
           </div>
 
           <div className="bg-white shadow-md p-6 rounded-xl">
           <h2 className="font-bold text-indigo-700 mb-6 text-lg">Job Description</h2>
-          <div className="mb-4">We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.</div>
+          <div className="mb-4">{job.description}</div>
           <h2 className="font-bold text-indigo-700 mb-3 text-lg">Salary</h2>
-          <div className="mb-4">$70k - $80K / Year</div>
+          <div className="mb-4">{job.salary}</div>
           </div>
 
           <div className="bg-white shadow-md p-6 rounded-xl mt-6">
             <h2 className="font-bold text-black mb-6 text-xl">Company Info</h2>
-            <h2 className=" mb-3 text-2xl">NewTek Solutions</h2>
-            <div className="pb-4 border-b">NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment. </div>
+            <h2 className=" mb-3 text-2xl">{job.company.name}</h2>
+            <div className="pb-4 border-b">{job.company.description}</div>
 
             <h2 className="mb-2 text-xl mt-3">Contact Email:</h2>
-            <div className="p-2 bg-indigo-100 font-bold">contact@newteksolutions.com</div>
+            <div className="p-2 bg-indigo-100 font-bold">{job.company.contactEmail}</div>
 
             <h2 className="mb-2 text-xl mt-3">Contact Phone:</h2>
-            <div className="p-2 bg-indigo-100 font-bold mb-1">56734-58478-4898</div>
+            <div className="p-2 bg-indigo-100 font-bold mb-1">{job.company.contactPhone}</div>
           </div>
 
           <div className="bg-white shadow-md p-6 rounded-xl mt-6">
@@ -56,8 +84,8 @@ const JobPage = () => {
 
 
       </section>
-
-
+    </>
+    )}
     </>
   )
 }
